f2a3188ae7866b9bb1ae970d3b7f978f8237367f
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-10 19:50:59 +0600

    add getMe route for authenticated users

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -6,6 +6,8 @@ const router = Router();
 
 router.post('/create-user', UserController.registerUser);
 
+router.get('/me', auth('user', 'admin'), UserController.getMe);
+
 router.get('/all-users', auth('user', 'admin'), UserController.getAllUser);
 
 export const UserRoutes = router;
