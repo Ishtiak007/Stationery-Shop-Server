c15f7b02b6c40f0d0ed80f519fcf0a4d1a9c3e1c
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-10 12:03:57 +0600

    add Zod validation schemas for authentication

---
+++ src/app/modules/auth/auth.validation.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+
+// Login
+const loginValidationSchema = z.object({
+  email: z.string({ required_error: 'email is required' }).email(),
+  password: z.string({ required_error: 'password is required' }),
+});
+
+// Change Password
+const ChangePassValidationSchema = z.object({
+  oldPassword: z.string({ required_error: 'Old password is required' }),
+  newPassword: z.string({ required_error: 'password is required' }),
+});
+
+const refreshTokenValidationSchema = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: 'Refresh token is required!',
+    }),
+  }),
+});
+
+export const AuthValidations = {
+  refreshTokenValidationSchema,
+  loginValidationSchema,
+  ChangePassValidationSchema,
+};
