84366ec8e322d6a199d76c857da5b8ef69f587b8
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-10 19:32:19 +0600

    Users retrieved Successfully functionality implemented

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,10 +1,12 @@
 import { TUser } from './user.interface';
 import { UserModel } from './user.model';
 
+// create a user
 const registerUser = async (payload: TUser) => {
   return await UserModel.create(payload);
 };
 
+// get all users
 const getAllUsersFromDB = async () => {
   return await UserModel.find();
 };
