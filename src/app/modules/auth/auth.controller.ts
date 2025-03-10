9ec4e5f064b815bcce4ca1e5c7578982e367ad75
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-10 13:49:23 +0600

    implement login functionality with access and refresh token handling

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import catchAsync from '../../../utility/catchAsync';
+import sendResponse from '../../../utility/sendResponse';
+import config from '../../config';
+
+const loginUser = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body);
+  const { accesToken, refreshToken } = result;
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.node_env === 'production',
+    httpOnly: true,
+    sameSite: 'none',
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+  });
+
+  sendResponse(res, {
+    statusCode: accesToken ? 200 : 500,
+    success: true,
+    message: accesToken ? 'login successful' : 'you are not an authorized user',
+    data: accesToken ? { token: accesToken } : [],
+  });
+});
+
+export const AuthController = {
+  loginUser,
+};
