a97698f54d9ca31358f474d3bac0d0a478f25bcd
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-10 19:28:12 +0600

    add getAllUsersFromDB service to fetch all users

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -5,6 +5,11 @@ const registerUser = async (payload: TUser) => {
   return await UserModel.create(payload);
 };
 
+const getAllUsersFromDB = async () => {
+  return await UserModel.find();
+};
+
 export const UserServices = {
   registerUser,
+  getAllUsersFromDB,
 };
