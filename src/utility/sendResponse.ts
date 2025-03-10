cac0fe2c68118b8c4cbd07f8717a8888cf4a28b2
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-10 11:35:29 +0600

    implement generic response handler

---
+++ src/utility/sendResponse.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Response } from 'express';
+
+type TResponse<T> = {
+  statusCode: number;
+  success: boolean;
+  message?: string;
+  data?: T;
+  meta?: any;
+};
+const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+  res.status(data?.statusCode).json({
+    success: data?.success,
+    message: data?.message,
+    meta: data.meta,
+    data: data.data,
+  });
+};
+export default sendResponse;
