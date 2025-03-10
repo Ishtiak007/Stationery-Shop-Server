59845de15516d801b597f5f901696b3268144e93
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-10 19:54:10 +0600

    implement getMe service to fetch authenticated user details based on role

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,4 +1,4 @@
-import { TUser } from './user.interface';
+import { TRole, TUser } from './user.interface';
 import { UserModel } from './user.model';
 
 // create a user
@@ -11,7 +11,20 @@ const getAllUsersFromDB = async () => {
   return await UserModel.find();
 };
 
+// get me
+const getMe = async (email: string, role: TRole) => {
+  let result = null;
+  if (role === 'user') {
+    result = await UserModel.findOne({ email });
+  }
+  if (role === 'admin') {
+    result = await UserModel.findOne({ email });
+  }
+  return result;
+};
+
 export const UserServices = {
   registerUser,
   getAllUsersFromDB,
+  getMe,
 };
