477809afeff66dacacf14f890e23f62bf14db785
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-11 14:28:42 +0600

    add product creation route with file upload and validation

---
+++ src/app/modules/products/product.routes.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import auth from '../../../middlewares/auth';
+import { upload } from '../../../utility/sendImageToCloudinary';
+import validateRequest from '../../../middlewares/ValidateRequest';
+
+const router = Router();
+
+//create product
+router.post(
+  '/create-product',
+  auth('admin'),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
+  validateRequest(ProductValidations.createProductValidation),
+  ProductControllers.createProduct,
+);
+export const ProductRoutes = router;
