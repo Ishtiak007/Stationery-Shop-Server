e15df99294b318ba17fa32ccfccf5cd5c939af0b
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-12 11:53:23 +0600

    add product routes to main application router

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { UserRoutes } from '../app/modules/user/user.routes';
 import { AuthRoutes } from '../app/modules/auth/auth.routes';
+import { ProductRoutes } from '../app/modules/products/product.routes';
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/users',
     route: UserRoutes,
+  },
+  {
+    path: '/products',
+    route: ProductRoutes,
   },
 ];
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
