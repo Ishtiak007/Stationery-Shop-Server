46de92aebc004dbc80f7b5d4a0ee8b58396f9924
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-12 12:26:52 +0600

    add product retrieval service with search, filter, sort, and pagination

--- src/app/modules/products/product.service.ts
+++ src/app/modules/products/product.service.ts
@@ -1,19 +1,35 @@
+import QueryBuilder from '../../../builder/Querybuilder';
 import { sendImageToCloudinary } from '../../../utility/sendImageToCloudinary';
+import { stationeryProductSearchableFields } from './product.constant';
 import { TProduct } from './product.interface';
 import { ProductModel } from './product.model';
 
+// create a product
 const createProductIntoDb = async (file: any, payload: TProduct) => {
   if (file) {
     const imageName = `$${payload.name}`;
     const path = file?.path;
-
-    //send image to cloudinary
     const { secure_url } = await sendImageToCloudinary(imageName, path);
     payload.productImg = secure_url as string;
   }
   return await ProductModel.create(payload);
 };
 
+// get all product from db
+const getAllProductFromDB = async (query: Record<string, unknown>) => {
+  const products = new QueryBuilder(ProductModel.find(), query)
+    .search(stationeryProductSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await products.modelQuery;
+  const meta = await products.countTotal();
+  return { result, meta };
+};
+
 export const productServices = {
   createProductIntoDb,
+  getAllProductFromDB,
 };
