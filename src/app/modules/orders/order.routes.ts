3d11c3353ec18cf72b49ac1b999c5be8bfa9befd
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-14 15:07:33 +0600

    add route for creating orders with admin and user authorization

--- src/app/modules/orders/order.routes.ts
+++ src/app/modules/orders/order.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import auth from '../../../middlewares/auth';
 
 const router = Router();
+
+//create order
+router.post('/', auth('admin', 'user'), OrderControllers.createOrder);
 
 export const OrderRoutes = router;
